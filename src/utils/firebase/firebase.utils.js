4875485261bc7b6ce2e5d37070f7552f3f2af49c
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-19 19:30:41 +0200

    switching from redux to redux-toolkit

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyCcy0r1sgJUpmHHHogpfPaXkH0vmN4VhYU",
+  authDomain: "crown-clothing-v2-fc8a9.firebaseapp.com",
+  projectId: "crown-clothing-v2-fc8a9",
+  storageBucket: "crown-clothing-v2-fc8a9.appspot.com",
+  messagingSenderId: "1017974224656",
+  appId: "1:1017974224656:web:73680fb6473c0ff2d7d795"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
