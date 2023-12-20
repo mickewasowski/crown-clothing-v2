845692d23eac9e81345ccf83e69d70b885b6d098
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-20 15:00:36 +0200

    added initial stripe configuration and created the payment form component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import App from './App';
 import { store, persistor } from './store/store';
-
 import './index.scss';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 const rootElement = document.getElementById('root');
 
@@ -16,7 +16,9 @@ render(
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
