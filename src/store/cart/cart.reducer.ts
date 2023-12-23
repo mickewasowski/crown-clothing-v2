4e4cad6978cde21ce7a9ea0699e909bca544acca
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-23 19:09:49 +0200

    typed the user files

--- src/store/cart/cart.reducer.ts
+++ src/store/cart/cart.reducer.ts
@@ -13,14 +13,14 @@ export const CART_INITIAL_STATE: CartState = {
 };
 
 export const cartReducer = (state = CART_INITIAL_STATE, action: AnyAction): CartState => {
-  if (setCartItems.match(action.type)) {
+  if (setCartItems.match(action)) {
     return {
       ...state,
       cartItems: action.payload,
     };
   }
 
-  if (setIsCartOpen.match(action.type)) {
+  if (setIsCartOpen.match(action)) {
     return {
       ...state,
       isCartOpen: action.payload,
