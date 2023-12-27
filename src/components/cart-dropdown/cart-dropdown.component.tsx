c4dc572b0a649776a108d494d5d8e47c7b14c930
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-27 22:39:27 +0200

    added useCallback as code optimization

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,23 +1,22 @@
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
 import { selectCartItems } from '../../store/cart/cart.selector';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
 } from './cart-dropdown.styles';
+import { useCallback } from 'react';
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
+  const goToCheckoutHandler = useCallback(() => {
     navigate('/checkout');
-  };
+  }, []);
 
   return (
     <CartDropdownContainer>
